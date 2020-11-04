7e3b3a25af857d0d57e0fbfe686780299331a566
Guilherme Barreto <barretogui90@gmail.com>
2020-11-04 16:57:36 -0300

    Init

---
+++ Aula012.py
@@ -0,0 +1,11 @@
+nome = str(input("Qual é seu nome??"))
+if nome == "Gui":
+    print("Que nome bonito")
+elif nome == "Ozzy":
+    print("Princípe das trevas,")
+elif nome == "Marcio Vera Jorge Matheus":
+    print("Conheço alguém com o mesmo nome!")
+else:
+    print("Nunca ouvi falar")
+print("tenha um bom dia, {}!" .format(nome))
+    
---
+++ ex036.py
@@ -0,0 +1,13 @@
+print("-=-" * 11)
+print(" ANALISADOR DE CRÉDITO APROVADO")
+print("-=-" * 11)
+casa = float(input("Qual o valos da casa? "))
+salario = float(input("Qual o seu salário? "))
+anos = float(input("Em quantos anos pretende pagar?" ))
+meses = (anos * 12) 
+credito = salario * 0.3
+parcela = casa / meses
+if credito >= parcela:
+    print('PARABÉNS SEU CRÉDITO FOI APROVADO!!! Será {:.0f} parcelas de R${:.2f} o seu financiamento !' .format(meses, parcela))
+else:
+    print('CRÉDITO NEGADO ! Infelizmente não foi aprovado para financiamento')
