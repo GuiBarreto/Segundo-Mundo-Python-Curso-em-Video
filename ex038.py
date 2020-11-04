21cd4d75c066e24b25e86de44bd9eb3a48c27632
Guilherme Barreto <barretogui90@gmail.com>
2020-11-04 17:35:41 -0300

    Create ex038.py

---
+++ ex038.py
@@ -0,0 +1,8 @@
+valor1 = float(input('Primeiro número: '))
+valor2 = float(input('Segundo número: '))
+if valor1 > valor2:
+    print('O primeiro número é maior!')
+elif valor1 < valor2:
+    print('O segundo valor é maior!')
+else:
+    print('Os dois números são iguais!')
